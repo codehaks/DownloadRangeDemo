580c1725598a9789bc5283373366d5dd254e282a
Hakim Ghods <ghods.hakim@hotmail.com>
2018-06-02 01:07:41 +0430

    Log range values

--- ServerApp/Controllers/HomeController.cs
+++ ServerApp/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace ServerApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger _logger;
-        
+
         private readonly string mediaType = "application/x-rar-compressed";
         private readonly string path;
 
@@ -36,10 +36,41 @@ namespace ServerApp.Controllers
         [Route("range")]
         public IActionResult DownloadRange()
         {
-            _logger.LogWarning("Range ---> Download started at {} \n ", DateTime.Now.TimeOfDay);
+            _logger.LogWarning($"Range ---> Download started at {DateTime.Now.TimeOfDay} \n ");
 
             var fileInfo = new FileInfo(path);
             return PhysicalFile(path, mediaType, "DownloadRang.zip", true);
+        }
+
+        [Route("range2")]
+        public IActionResult DownloadRange2()
+        {
+            var range = HttpContext.Request.Headers["Range"];
+
+            if (!string.IsNullOrEmpty(range))
+            {
+                var result = GetRange(range);
+
+                _logger.LogWarning($"Range ---> Download started at {DateTime.Now.TimeOfDay} from {result.Item1} \n ");
+            }
+
+            var fileInfo = new FileInfo(path);
+            return PhysicalFile(path, mediaType, "DownloadRang.zip", true);
+        }
+
+        private Tuple<long, long> GetRange(string range)
+        {
+            // bytes=1213-4545
+
+            var b = range.Split("=");
+            var s = b[1].Split("-");
+
+            if (string.IsNullOrEmpty(s[1]))
+            {
+                s[1] = "0";
+            }
+
+            return new Tuple<long, long>(Convert.ToInt64(s[0]), Convert.ToInt64(s[1]));
         }
     }
 }
